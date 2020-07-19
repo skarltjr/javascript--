4e398f3abae0817255bc36f72ddee525b394ea49
skarltjr <62214428+skarltjr@users.noreply.github.com>
2020-07-19 18:58:59 +0900

    plus

--- momentum-clone/clock.js
+++ momentum-clone/clock.js
@@ -5,10 +5,10 @@ function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  const sec = date.getSeconds();
+
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
-  }:${sec < 10 ? `0${sec}` : sec}`;
+  }`;
 }
 
 function init() {
---
+++ momentum-clone/weather.js
@@ -0,0 +1,52 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "9657934c0272d0e678c7eee533730fdb";
+const COORDS = `coords`;
+
+function getWeather(lat, long) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature}º ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude: latitude,
+    longitude: longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords == null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+init();
